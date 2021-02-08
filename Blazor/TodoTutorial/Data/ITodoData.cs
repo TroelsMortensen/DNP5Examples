c870ff924e3c463f75af8f89148131cc7b8624aa
TRMO <tromortensen@hotmail.com>
2021-02-08 16:52:47 +0100

    todo tut done

---
+++ Blazor/TodoTutorial/Data/ITodoData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TodoTutorial.Models;
+
+namespace TodoTutorial.Data
+{
+    public interface ITodoData
+    {
+        IList<Todo> GetTodos();
+        void AddTodo(Todo todo);
+        void RemoveTodo(int todoId);
+        void Update(Todo todo);
+        Todo Get(int id);
+    }
+}
---
+++ RabbitMQ/HelloWorldServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Receiver().Start();
+        }
+    }
+}
