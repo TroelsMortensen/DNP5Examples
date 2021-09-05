dac090157259c213dc5f736522f227340b8f465c
TRMO <tromortensen@hotmail.com>
2021-09-05 09:46:55 +0200

    cleaning up

--- EFC/IncludeExample/Models/Address.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IncludeExample.Models
-{
-    public class Address
-    {
-        public string Street { get; set; }
-        public int HouseNumber { get; set; }
-        public int PostCode { get; set; }
-    }
-}
--- JSON/JSON/OtherObj.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JSON
-{
-    public class OtherObj
-    {
-        public string Name { get; set; }
-        public int OtherNumber { get; set; }
-        public bool ThisIsMyBoolean { get; set; }
-    }
-}
